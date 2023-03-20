cb50c502b6f742f2ea93d9410abc809a98ab8c2e
Masa Orel <orel.masha89@gmail.com>
2023-03-20 22:05:40 +0100

    added personal info with inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,96 @@
-import { useState } from 'react'
-import './App.css'
+import { first } from "lodash";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    address: "",
+    city: "",
+    country: "",
+    phone: "",
+    email: "",
+  });
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setFormData({
+      ...formData,
+      [e.target.name]: value,
+    });
+  }
 
   return (
     <div className="App">
-     
+      {/* header, main with two divs and footer */}
+      <div className="data-container">
+        <h1>Personal information</h1>
+        <div className="data-input">
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First name"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last name"
+            onChange={handleChange}
+          />
+          <input
+            type="file"
+            name=""
+            placeholder="Browse image"
+            onChange={(e) => {
+              console.log(e.target.files[0]);
+            }}
+          />
+          <input
+            type="text"
+            name="address"
+            placeholder="Street name"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="city"
+            placeholder="City"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="country"
+            placeholder="Country"
+            onChange={handleChange}
+          />
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Phone number"
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={handleChange}
+          />
+          <textarea
+            name="description"
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Description"
+            onChange={handleChange}
+          ></textarea>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
